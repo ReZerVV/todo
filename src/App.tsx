870141edbde4fd2c95ca42d5940b29b2e41326c9
Kiril Morozov <47004667+ReZerVV@users.noreply.github.com>
2023-05-26 21:19:05 +0300

    created todo application

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,72 @@
 import React, { useState } from 'react';
 import './App.css'
-import { Item } from './components/Item';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+class Task {
+    constructor(
+        public id: number,
+        public title: string,
+        public status: boolean = false
+    ) { }
+}
+
+
 
 function App() {
-    const [todo, setTodo] = useState('')
-    const [todoList, setTodoList] = useState<string[] | []>([])
+    const [todo, setTodo] = useState<string>('');
+    const [tasks, setTasks] = useState<Task[] | []>([
+        new Task(0, 'test'),
+        new Task(1, 'test2', true),
+    ]);
 
-    let AddTodo = () => {
-        setTodoList([...todoList, todo]);
+    let AddTask = () => {
+        setTasks([...tasks, new Task(tasks.length, todo)])
+        setTodo('');
     }
+    let DoneTask = (id: number) => {
+        tasks.forEach((task) => {
+            if (task.id == id) {
+                task.status = true;
+            }
+        });
+        setTasks([...tasks]);
+    }
+    let DeleteTask = (id: number) => {
+        setTasks([...tasks.filter((task) => task.id != id)]);
+    }
+
     return (
         <section className='todo'>
-            <div>
-                <div className='todo-action'>
-                    <input className='todo-action-input'
-                           type='text'
-                           onChange={e => setTodo(e.target.value)}
-                           value={todo}/>
-                    <button onClick={AddTodo}
-                            className='todo-action-add'>Add</button>
-                </div>
+            <div className='todo-action'>
+                <input type='text'
+                       className='todo-action-input'
+                       onChange={e => setTodo(e.target.value)}
+                       value={todo}/>
+                <button className='todo-action-newtask'
+                        onClick={AddTask}>New Task</button>
             </div>
             <div className='todo-list'>
-                { todoList.map(i => <Item description={i}/>) }
+                {tasks && tasks.length ? '' : <p className='message'>No Tasks...</p>}
+                {tasks && tasks.map((task, index) => {
+                    return (
+                        <React.Fragment>
+                            <div className='task-bg'>
+                                <div className={task.status ? 'task__done' : 'task'}>
+                                    <span className='task-id'>{index}</span>
+                                    <span className='task-title'>{task.title}</span>
+                                </div>
+                                <div className='task-action'>
+                                    { task.status ? '' : 
+                                    <span title='done'
+                                          onClick={() => DoneTask(task.id)}><FontAwesomeIcon icon={faCheck} /></span> }
+                                    <span title='delete'
+                                          onClick={() => DeleteTask(task.id)}><FontAwesomeIcon icon={faXmark} /></span>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                    )
+                })}
             </div>
         </section>
     );
